b8e516a6f7edafe2b1d47e9ffe2ee6e1e4eb4fab
Med Jam <elqajjam@gmail.com>
2024-03-18 08:47:11 +0000

    Add Doc

--- descrimanator.py
+++ descrimanator.py
@@ -3,6 +3,14 @@ import torch.nn as nn
 
 class CNNBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=2):
+        """
+        CNNBlock class represents a convolutional block in a neural network.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            stride (int, optional): Stride value for the convolutional layer. Defaults to 2.
+        """
         super().__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 4, stride, 1, bias=False, padding_mode="reflect"),
@@ -11,10 +19,35 @@ class CNNBlock(nn.Module):
         )
     
     def forward(self, x):
+        """
+        Forward pass of the CNNBlock.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after passing through the convolutional block.
+        """
         return self.conv(x)
     
 
 class Descriminator(nn.Module):
+    """
+    The Descriminator class represents the discriminator network in the Pix2Pix model.
+
+    Args:
+        in_channels (int): Number of input channels. Default is 3.
+        features (list): List of integers representing the number of features in each layer. Default is [64, 128, 256, 512].
+
+    Attributes:
+        initial (nn.Sequential): Initial layers of the discriminator network.
+        model (nn.Sequential): Main model of the discriminator network.
+
+    Methods:
+        forward(x, y): Forward pass of the discriminator network.
+
+    """
+
     def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
         super().__init__()
         self.initial = nn.Sequential(
@@ -35,6 +68,17 @@ class Descriminator(nn.Module):
         self.model = nn.Sequential(*layers)
         
     def forward(self, x, y):
+        """
+        Forward pass of the discriminator network.
+
+        Args:
+            x (torch.Tensor): Input tensor representing the real image.
+            y (torch.Tensor): Input tensor representing the generated image.
+
+        Returns:
+            torch.Tensor: Output tensor representing the discriminator's prediction.
+
+        """
         x = torch.cat([x, y], dim = 1)
         x = self.initial(x)
         return self.model(x)
